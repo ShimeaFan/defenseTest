ffdefe955741d70a6497574f06aa8665bff2fee9
shfanj <1145483130@qq.com>
2020-05-08 10:42:02 +0800

    commit

---
+++ src/main/java/com/practice/test/entity/UserEntity.java
@@ -0,0 +1,17 @@
+package com.practice.test.entity;
+
+import lombok.Data;
+
+/**
+ * @author fanShiHao
+ * @date 2020/5/8 10:04
+ * @description 用户实体
+ */
+@Data
+public class UserEntity {
+
+    private String name;
+    private int age;
+    private String address;
+
+}
